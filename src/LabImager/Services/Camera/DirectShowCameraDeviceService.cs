a558a053ec7fbc4af942e540564bc58d29ac34c6
Darren McGann <dmcgann@msetech.org>
2026-06-03 12:19:06 -0400

    Refine Lab Imager UI layout and add notes editor toolbar

---
+++ src/LabImager/Services/Camera/DirectShowCameraDeviceService.cs
@@ -0,0 +1,21 @@
+﻿using DirectShowLib;
+using LabImager.Models.Camera;
+
+namespace LabImager.Services.Camera;
+
+public sealed class DirectShowCameraDeviceService : ICameraDeviceService
+{
+public IReadOnlyList<CameraDeviceInfo> GetAvailableDevices()
+{
+var devices = DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice);
+
+    return devices
+        .Select(device => new CameraDeviceInfo
+        {
+            Name = device.Name,
+            DevicePath = device.DevicePath
+        })
+        .ToList();
+}
+
+}
