01ecd36c12c38e67bf32f1fe0d14f89a0472b67b
Darren McGann <dmcgann@msetech.org>
2026-06-03 14:46:27 -0400

    Add preview service scaffold

---
+++ src/LabImager/Services/Preview/ICameraPreviewService.cs
@@ -0,0 +1,12 @@
+﻿using LabImager.Models.Camera;
+
+namespace LabImager.Services.Preview;
+
+public interface ICameraPreviewService
+{
+    bool IsPreviewRunning { get; }
+
+    void StartPreview(CameraDeviceInfo source);
+
+    void StopPreview();
+}
---
+++ src/LabImager/Services/Preview/StubCameraPreviewService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using LabImager.Models.Camera;
+
+namespace LabImager.Services.Preview;
+
+public sealed class StubCameraPreviewService : ICameraPreviewService
+{
+    public bool IsPreviewRunning { get; private set; }
+
+    public CameraDeviceInfo? ActiveSource { get; private set; }
+
+    public void StartPreview(CameraDeviceInfo source)
+    {
+        if (string.IsNullOrWhiteSpace(source.Name))
+        {
+            throw new ArgumentException("Cannot start preview without a valid source name.", nameof(source));
+        }
+
+        ActiveSource = source;
+        IsPreviewRunning = true;
+    }
+
+    public void StopPreview()
+    {
+        ActiveSource = null;
+        IsPreviewRunning = false;
+    }
+}
