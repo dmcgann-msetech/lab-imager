2e82b02c2114c05ef033fe1a065192290c243099
Darren McGann <dmcgann@msetech.org>
2026-06-05 17:10:53 -0400

    Add recording service foundation

---
+++ src/LabImager/Services/Recording/IRecordingService.cs
@@ -0,0 +1,16 @@
+﻿namespace LabImager.Services.Recording;
+
+public interface IRecordingService
+{
+    RecordingState State { get; }
+    TimeSpan Elapsed { get; }
+
+    event EventHandler? StateChanged;
+    event EventHandler? ElapsedChanged;
+
+    void Start();
+    void Pause();
+    void Resume();
+    void Stop();
+    void Reset();
+}
